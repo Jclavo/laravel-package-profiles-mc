f62e8d2a7fa3315a3ae7cd120dd3ee0feae6af6f
jose clavo <jclavotafur@gmail.com>
2022-09-28 18:34:03 -0300

    add use

--- src/Models/Profile.php
+++ src/Models/Profile.php
@@ -4,7 +4,7 @@ namespace Jclavo\Profiles\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-// use Jclavo\Profiles\Database\Factories\ProfileFactory;
+use Jclavo\Profiles\Database\Factories\ProfileFactory;
 
 class Profile extends Model
 {
@@ -19,7 +19,7 @@ class Profile extends Model
      */
     protected static function newFactory()
     {
-        return new \Jclavo\Profiles\Database\Factories\ProfileFactory;
+        return new ProfileFactory;
     }
 
     /**
